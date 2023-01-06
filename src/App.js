6ea9ef9c8f905d0b1fc24dff31f49359138e0204
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2023-01-06 10:28:04 -0300

    Alterando para SSL o backend e removendo o h1 desnecessário

--- src/App.js
+++ src/App.js
@@ -4,15 +4,9 @@ import CourseList from "./component/CourseList/CourseList";
 
 function App() {
   return (
-    <Fragment>
-      <h1>
-        Variável {process.env.NODE_ENV}/{process.env.REACT_APP_ENVORIMENT}
-      </h1>
-      <br />
-      <div className="centerpage">
-        <CourseList />
-      </div>
-    </Fragment>
+    <div className="centerpage">
+      <CourseList />
+    </div>
   );
 }
 
